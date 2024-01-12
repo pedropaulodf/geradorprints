3076256f7d5844ee944261e11e9a3e3f4910ef80
Pedro Paulo <pedropaulotj@gmail.com>
2024-01-12 16:46:45 -0300

    feat: Tela Débitos - Atualização layout

--- src/pages/iPadScreens/iPhoneScreensCOPIA/ScreenDetalheCobranca/index.tsx
+++ src/pages/iPadScreens/iPhoneScreensCOPIA/ScreenDetalheCobranca/index.tsx
@@ -1,7 +1,8 @@
 import AppHeader from "../../../../components/AppHeader";
 import IphoneBottomLine from "../../../../components/IphoneBottomLine";
-import { useSettingsContext } from "../../../../contexts/useSettingsContext";
-import { FiCopy, FiX } from "react-icons/fi";
+import { FiCopy, FiX, FiChevronDown, FiChevronUp } from "react-icons/fi";
+import { MdQrCode2 } from "react-icons/md";
+import { RiBarcodeFill } from "react-icons/ri";
 
 import bancoVakeLogoSvg from "../../../../assets/banco-fake.svg";
 
@@ -43,25 +44,64 @@ export default function ScreenDetalheCobranca({ isIpad = false }: PropsType) {
           <p className="text">{`10/07/${new Date().getFullYear()}`}</p>
         </div>
         <hr />
-        <div className="horizontal-items bank-box">
-          <p className="label">
-            <strong>Banco Emissor:</strong>
-          </p>
-          <img src={bancoVakeLogoSvg} alt="Banco logo" className="bank-logo" />
+        <div
+          className="payment-accordeon-container"
+          style={{ marginTop: "1.3em" }}
+        >
+          <div className="payment-accordeon-container-header">
+            <div className="payment-accordeon-container-header-left">
+              <MdQrCode2 color="#383838" size={60} />
+              <p>PIX</p>
+            </div>
+            <div className="payment-accordeon-container-header-right">
+              <FiChevronDown color="#383838" size={60} />
+            </div>
+          </div>
         </div>
-        <hr />
 
-        <div className="typing-line-container">
-          <p>Linha Digitável</p>
-          <input
-            type="text"
-            value="53010800004238200000078115433490400039000"
-            onChange={() => {}}
-          />
-          <button type="button">
-            <FiCopy color="#ffffff" size={42} />
-            Copiar
-          </button>
+        <div className="payment-accordeon-container">
+          <div
+            className="payment-accordeon-container-header"
+            style={{
+              backgroundColor: "#dfdfdf",
+              borderRadius: "16px 16px 0px 0px",
+            }}
+          >
+            <div className="payment-accordeon-container-header-left">
+              <RiBarcodeFill color="#383838" size={60} />
+
+              <p>Boleto</p>
+            </div>
+            <div className="payment-accordeon-container-header-right">
+              <FiChevronUp color="#383838" size={60} />
+            </div>
+          </div>
+          <div className="payment-accordeon-container-content">
+            <div className="horizontal-items bank-box">
+              <p className="label">
+                <strong>Banco Emissor:</strong>
+              </p>
+              <img
+                src={bancoVakeLogoSvg}
+                alt="Banco logo"
+                className="bank-logo"
+              />
+            </div>
+            <hr />
+
+            <div className="typing-line-container">
+              <p>Linha Digitável</p>
+              <input
+                type="text"
+                value="53010800004238200000078115433490400039000"
+                onChange={() => {}}
+              />
+              <button type="button">
+                <FiCopy color="#ffffff" size={42} />
+                Copiar Código de Barras
+              </button>
+            </div>
+          </div>
         </div>
       </div>
       {!isIpad && <IphoneBottomLine />}
